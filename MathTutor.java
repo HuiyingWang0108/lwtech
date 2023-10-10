49795ae6932a37fab6d4eb2b30a4a7e725aa44f1
Huiying Wang <whymissu1228@gmail.com>
2023-10-09 17:33:45 -0700

    updated MathTutor.java

--- MathTutor.java
+++ MathTutor.java
@@ -13,16 +13,16 @@ public class MathTutor {
         // initialize a Random object
         Random rnd = new Random();
 
-        int choice, num1, num2, reponse, answer;
-        
-        do {
+        int num1, num2, answer;
+        String choice, reponse;
+
+        while (true) {
             System.out.println("Math Tutor");
             System.out.println("1. Addition problem\n" +
                     "2. Subtraction problem\n" +
                     "3. Quit");
             System.out.print("Enter your choice (1 - 3):");
 
-            choice = scanner.nextInt();
             // generate a random integer from 0 to 999
             num1 = rnd.nextInt(1000);
             num2 = rnd.nextInt(1000);
@@ -33,32 +33,25 @@ public class MathTutor {
                 num2 = tempNum;
             }
             String lineString = "--------";
-
-            switch (choice) {
-                case 1:
-                    answer = num1 + num2;
-                    System.out.printf("%8d%n+%7d%n%s%n", num1, num2, lineString);
-                    reponse = scanner.nextInt();
-                    checkAnswerCorrect(answer, reponse);
-                    break;
-                case 2:
-                    answer = num1 - num2;
-                    System.out.printf("%8d%n-%7d%n%s%n", num1, num2, lineString);
-                    reponse = scanner.nextInt();
-                    checkAnswerCorrect(answer, reponse);
-                    break;
-                case 3:
-                    break;
-                default:
-                    System.out.println("ERROR!\n");
-
+            choice = scanner.next();
+            if (choice.equals("1")) {
+                answer = num1 + num2;
+                System.out.printf("%8d%n+%7d%n%s%n", num1, num2, lineString);
+                reponse = scanner.next();
+                checkAnswerCorrect(answer, reponse);
+            } else if (choice.equals("2")) {
+                answer = num1 - num2;
+                System.out.printf("%8d%n-%7d%n%s%n", num1, num2, lineString);
+                reponse = scanner.next();
+                checkAnswerCorrect(answer, reponse);
+            } else if (choice.equals("3")) {
+                break;
             }
-        } while (choice != 3);
-
+        }
     }
 
-    private static void checkAnswerCorrect(int answer, int reponse) {
-        if (reponse == answer) {
+    private static void checkAnswerCorrect(int answer, String reponse) {
+        if (reponse.equals(answer)) {
             System.out.println("Congratulations!\n");
         } else {
             System.out.println("The correct answer is: " + answer + "\n");
